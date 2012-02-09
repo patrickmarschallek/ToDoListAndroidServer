758e39aed82fd26151e766704f5c14423a164477
Patrick Marschallek <patrick.marschallek@googlemail.com>
2012-02-09 20:34:52 +0100

    adding entity layer

---
+++ src/de/fhb/mobile/toDoList/entity/Contact.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package de.fhb.mobile.toDoList.entity;
+
+
+/**
+ * @author Patrick
+ *
+ */
+public class Contact {
+
+	private int id;
+	/**
+	 * contactId is the id from the android directory.
+	 */
+	private int contactId;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the contactId
+	 */
+	public int getContactId() {
+		return contactId;
+	}
+	/**
+	 * @param contactId the contactId to set
+	 */
+	public void setContactId(int contactId) {
+		this.contactId = contactId;
+	}
+	
+	
+}
---
+++ src/de/fhb/mobile/toDoList/entity/Todo.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package de.fhb.mobile.toDoList.entity;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Patrick
+ * 
+ */
+public class Todo {
+	private int id;
+	private String name;
+	private String description;
+	private boolean finished;
+	private Date expires;
+	private User user;
+	private List<Contact> contacts;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the finished
+	 */
+	public boolean isFinished() {
+		return finished;
+	}
+
+	/**
+	 * @param finished
+	 *            the finished to set
+	 */
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
+
+	/**
+	 * @return the expires
+	 */
+	public Date getExpires() {
+		return expires;
+	}
+
+	/**
+	 * @param expires
+	 *            the expires to set
+	 */
+	public void setExpires(Date expires) {
+		this.expires = expires;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public User getUser() {
+		return user;
+	}
+
+	/**
+	 * @param user
+	 *            the user to set
+	 */
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	/**
+	 * @return the contacts
+	 */
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	/**
+	 * @param contacts the contacts to set
+	 */
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+}
